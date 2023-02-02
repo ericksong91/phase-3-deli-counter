b103fc04bd4de599756dbeb2f97434661b8b9e69
Eric Song <ericksong91@gmail.com>
2023-02-02 17:13:46 -0500

    commit tree bdfb38dddad78d9d1eba603e41e1b3ca9c7c02fb

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,36 @@
-# Write your code here.
+require 'pry'
+
+katz_deli = ["logam", "logan", "paul"]
+
+def line (deli)
+
+    if deli.length == 0 then
+        puts "The line is currently empty."
+    else
+        line = "The line is currently:"
+
+        deli.map.with_index(1) do |cust, index|
+            line << " #{index}. #{cust}"
+        end
+
+        puts line
+    end
+end
+
+def take_a_number(deli, person)
+    deli << person
+
+    puts "Welcome, #{deli.last}. You are number #{deli.rindex(deli.last) + 1} in line."
+
+end
+
+def now_serving(deli)
+    if deli.empty? 
+        puts "There is nobody waiting to be served!"
+    else 
+        puts "Currently serving #{deli.first}."
+
+        deli.shift
+    end
+
+end
